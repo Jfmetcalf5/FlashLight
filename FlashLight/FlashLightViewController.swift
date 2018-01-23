41eaaf5db3e579cd13ff52e1cf26cbe8230b87dd
Jfmetcalf5 <Jfmetcalf5@gmail.com>
2018-01-22 19:39:08 -0700

    Added a switch to toggle between white and black background

--- FlashLight/FlashLightViewController.swift
+++ FlashLight/FlashLightViewController.swift
@@ -16,18 +16,38 @@ class FlashLightViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+}
+    
+    @IBAction func lightSwitch(_ sender: UISwitch) {
+        if isOn {
+            isOn = false
+            view.backgroundColor = UIColor.black
+            lightButton.setTitleColor(.white, for: .normal)
+            lightButton.setTitle("Off", for: .normal)
+            UIApplication.shared.statusBarStyle = .lightContent
+            
+        } else {
+            isOn = true
+            view.backgroundColor = UIColor.white
+            lightButton.setTitleColor(.black, for: .normal)
+            lightButton.setTitle("On", for: .normal)
+        }
+
     }
-    @IBAction func swipedRight(_ sender: UISwipeGestureRecognizer) {
-        UIApplication.shared.statusBarStyle = .lightContent
-        view.backgroundColor = UIColor.black
-        lightButton.setTitleColor(.white, for: .normal)
-    }
-    @IBAction func swipedLeft(_ sender: UISwipeGestureRecognizer) {
-        UIApplication.shared.statusBarStyle = .default
-        view.backgroundColor = UIColor.white
-        lightButton.setTitleColor(.black, for: .normal)
-    }
+    
+    
+    
+    
+//    @IBAction func swipedRight(_ sender: UISwipeGestureRecognizer) {
+//        UIApplication.shared.statusBarStyle = .lightContent
+//        view.backgroundColor = UIColor.black
+//        lightButton.setTitleColor(.white, for: .normal)
+//    }
+//    @IBAction func swipedLeft(_ sender: UISwipeGestureRecognizer) {
+//        UIApplication.shared.statusBarStyle = .default
+//        view.backgroundColor = UIColor.white
+//        lightButton.setTitleColor(.black, for: .normal)
+//    }
     
 }
     
